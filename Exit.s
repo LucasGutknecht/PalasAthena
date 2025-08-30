9df92c5f9b50dea81dae8fa8481e6e5c7204be09
Lucas <lucas@albesbrasil.com.br>
2025-08-30 12:59:22 -0300

    Adicionado referencia.

--- Exit.s
+++ Exit.s
@@ -1,3 +1,4 @@
+// Usando como base: Programming from the Ground Up
 // Propósito: Programa simples que sai e retorna um código de saída para o kernel
 // Pode ser visualizado com: echo $?
 // Para macOS em Apple Silicon (ARM64)
@@ -6,8 +7,8 @@
 .align 2
 
 _main:
-    mov x0, #0          // Código de retorno
-    ret                 // Retorna para o caller (libc)
+    mov x0, #0
+    ret
 
 // Para compilar e executar:
 // as -o exit_simple.o exit_simple.s
